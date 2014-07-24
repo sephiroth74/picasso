276b85fe30dc9589486115d207cdfb04ea4084e4
Alessandro Crugnola <alessandro@aviary.com>
2014-07-24 15:31:03 +0200

    targetwidth/targetheight used when the target view has visibility GONE (like when imageview is inside a viewanimator), otherwise the deferred creator never starts

--- picasso/src/main/java/it/sephiroth/android/library/picasso/RequestCreator.java
+++ picasso/src/main/java/it/sephiroth/android/library/picasso/RequestCreator.java
@@ -21,6 +21,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
 
@@ -602,8 +603,16 @@ public class RequestCreator {
       if (data.hasSize()) {
         throw new IllegalStateException("Fit cannot be used with resize.");
       }
-      int width = target.getWidth();
-      int height = target.getHeight();
+
+	  int width, height;
+	  if (target.getVisibility() == View.GONE) {
+		width = target.getMeasuredWidth();
+		height = target.getMeasuredHeight();
+	  } else {
+		width = target.getWidth();
+		height = target.getHeight();
+	  }
+
       if (width == 0 || height == 0) {
         setPlaceholder(target, placeholderResId, placeholderDrawable);
         picasso.defer(target, new DeferredRequestCreator(this, target, callback));
